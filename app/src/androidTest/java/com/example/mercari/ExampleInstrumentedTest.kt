063a2b201a1d5f182f466ba9d2ab8b6e18444d93
uetaku1125 <taku.ueda1125@gmail.com>
2021-09-13 23:52:17 +0900

    Add TestCode

--- app/src/androidTest/java/com/example/mercari/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/mercari/ExampleInstrumentedTest.kt
@@ -1,17 +1,18 @@
 package com.example.mercari
 
+import android.content.ClipData
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
 import org.junit.Assert.*
 
 
@@ -22,6 +23,7 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
@@ -29,6 +31,10 @@ class ExampleInstrumentedTest {
         assertEquals("com.example.mercari", appContext.packageName)
     }
 
+ //   @get:Rule
+ //   val activityRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
     fun registerAddress() {
         MainActivity().clickMyPageButton()
         MyPage().clickProfileButton()
@@ -37,23 +43,39 @@ class ExampleInstrumentedTest {
         RegAddress().inputAddress()
         AddressList().assertAddress()
     }
+
+    @Test
+    fun searchItem(){
+        MainActivity().inputSearchString()
+        SearchResult().clickItem()
+        ItemDetail().checkItemDetail()
+    }
+
 }
 
 class MainActivity(){
     fun clickMyPageButton(): MyPage{
         onView(withId(R.id.btn_mypage)).perform(click())
+        isDisplayed()
         return MyPage()
+    }
+
+    fun inputSearchString(){
+        onView(withId(R.id.search)).perform(ViewActions.typeText("MacBook"))
+        onView(withId(R.id.search)).perform(click())
     }
 }
 
 class MyPage() {
     fun clickProfileButton(): Profile{
         onView(withId(R.id.btn_profile)).perform(click())
+        isDisplayed()
         return Profile()
     }
 
     fun clickHomeButton(): MainActivity{
         onView(withId(R.id.btn_home)).perform(click())
+        isDisplayed()
         return MainActivity()
     }
 }
@@ -61,11 +83,13 @@ class MyPage() {
 class Profile() {
     fun clickAddressButton(): AddressList{
         onView(withId(R.id.btn_address)).perform(click())
+        isDisplayed()
         return AddressList()
     }
 
     fun clickBackButton(): MyPage{
         onView(withId(R.id.btn_back)).perform(click())
+        isDisplayed()
         return MyPage()
     }
 }
@@ -73,32 +97,46 @@ class Profile() {
 class AddressList() {
     fun clickRegAddressButton(): RegAddress{
         onView(withId(R.id.btn_regAddr)).perform(click())
+        isDisplayed()
         return RegAddress()
     }
 
-    fun assertAddress(): AddressList{
+    fun assertAddress(){
         onView(withId(R.id.textView_address)).check(
             matches(withText(EXTRA_MESSAGE))
         )
-        return this
     }
 
     fun clickBackProfileButton(): Profile{
         onView(withId(R.id.btn_backProfile)).perform(click())
+        isDisplayed()
         return Profile()
     }
 }
 
 class RegAddress() {
-    fun inputAddress(): RegAddress {
+    fun inputAddress(){
         onView(withId(R.id.editTextTextFamilyName)).perform(
             ViewActions.replaceText("test address")
         )
-        return this
     }
 
-    fun clickBackAddressButton(): AddressList {
+    fun clickBackAddressButton(): AddressList{
         onView(withId(R.id.btn_backAddress)).perform(click())
+        isDisplayed()
         return AddressList()
+    }
+}
+
+class SearchResult(){
+    fun clickItem(): ItemDetail{
+        onView(withId(R.id.list_item)).perform(click())
+        return ItemDetail()
+    }
+}
+
+class ItemDetail(){
+    fun checkItemDetail(){
+        onView(withId(R.id.textView_itemDetail)).check(matches(withText("macbook")))
     }
 }
